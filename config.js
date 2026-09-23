2b09d84f9a6e0ead831b3e28b86e5add8422335b
sevenronin66 <106922130+sevenronin66@users.noreply.github.com>
2026-09-23 06:57:37 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // ÚNICO dato que hay que cambiar: pegá entre las comillas el link de tu app web de Google
 // (el que termina en /exec, de Implementar > Gestionar implementaciones).
-window.PTAR_URL = 'PEGAR_AQUI_EL_LINK_EXEC';
+window.PTAR_URL = 'https://script.google.com/macros/s/AKfycbw6XX90gUfu3mH0fGXXCzHpcKgGfR4L_6ayn5LFUir730-A6vp9K0Q91PJr9hNSZDz6YQ/exec';
